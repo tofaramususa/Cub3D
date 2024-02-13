23fed61fd7c08e48f3b1f6ad4a9c30d5368fe498
jtofaramususa@gmail.com <jtofaramususa@gmail.com>
2024-02-13 23:49:20 +0400

    changes

--- 02_raycast.c
+++ 02_raycast.c
@@ -6,13 +6,14 @@
 /*   By: tofaramususa <tofaramususa@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 17:29:17 by tmususa           #+#    #+#             */
-/*   Updated: 2024/02/13 18:50:56 by tofaramusus      ###   ########.fr       */
+/*   Updated: 2024/02/13 23:37:28 by tofaramusus      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	initial_values(t_ray *ray, t_player *player) //this to calculate positions
+void	initial_values(t_ray *ray, t_player *player)
+// this to calculate positions
 {
 	// ray->deltaX = abs(1 / ray->rayDirX);
 	// ray->deltaY = abs(1 / ray->rayDirY);
@@ -22,12 +23,33 @@ void	initial_values(t_ray *ray, t_player *player) //this to calculate positions
 	ray->deltaY = abs(1 / ray->rayDirY);
 }
 
-int	wall_position(t_ray *ray)
+int	wall_position(t_ray *ray, t_player *player)
 {
 	int	hit;
 
 	char **realMap; // this is the actual map we have
 	hit = 0;
+	// calculate step and initial sideDist
+	if (ray->rayDirX < 0)
+	{
+		ray->stepX = -1;
+		ray->sideX = (player->pos_x - ray->mapX) * ray->deltaX;
+	}
+	else
+	{
+		ray->stepX = 1;
+		ray->sideX = (ray->mapX + 1.0 - player->pos_x) * ray->deltaX;
+	}
+	if (ray->rayDirY < 0)
+	{
+		ray->stepY = -1;
+		ray->sideY = (player->pos_y - ray->mapY) * ray->deltaY;
+	}
+	else
+	{
+		ray->stepY = 1;
+		ray->sideY = (ray->mapY + 1.0 - player->pos_y) * ray->deltaY;
+	}
 	while (hit = 0)
 	{
 		if (ray->sideX < ray->sideY)
@@ -46,7 +68,7 @@ int	wall_position(t_ray *ray)
 			// we are checking if we inside real map
 			hit = 1;
 	}
-	if (ray->side == 0) //not sure here
+	if (ray->side == 0) // not sure here
 		ray->perpWallDist = (ray->sideX - ray->deltaX);
 	else
 		ray->perpWallDist = (ray->sideY - ray->deltaY);
@@ -58,69 +80,82 @@ void	initiate_ray(t_ray *ray)
 }
 // for casting each ray
 
-//NEED FUNCTION TO IMPLEMENT RAY DRAW START AND END;
-void get_line_height(t_ray * ray) //to set draw_start and draw_end
+// NEED FUNCTION TO IMPLEMENT RAY DRAW START AND END;
+void	get_line_height(t_ray *ray) // to set draw_start and draw_end
 {
 	ray->line_height = (int)(WINDOW_HEIGHT / ray->perpWallDist);
 	ray->draw_start = -ray->line_height / 2 + WINDOW_HEIGHT / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
 	ray->draw_end = ray->line_height / 2 + WINDOW_HEIGHT / 2;
-	//we know the distance between the wall the cam vector
+	if (ray->draw_end >= WINDOW_HEIGHT)
+		ray->draw_end = WINDOW_HEIGHT - 1;
+	// we know the distance between the wall the cam vector
 }
 
-void	draw_wall(t_player *player, t_ray *ray, int current_x)
+void	draw_wall(t_data *root, t_player *player, t_ray *ray, int current_x)
 {
-	t_line	line;
-	double	wall_x; //what is wall x for 
+	t_line	*line;
 
-	if (ray->side == WEST || ray->side == EAST) //not sure here what side is?, wait maybe it is side the ray is hitting
-		wall_x = player->pos_y + ray->perpWallDist * ray->rayDirY;
-	else
-		wall_x = player->pos_x + ray->perpWallDist * ray->rayDirX;
-	wall_x -= floor(wall_x); //this is to get the texture x coordinate
-	line->x = current_x; //how do l find current
+	double wall_x; // what is wall x for
+	// if (ray->side == WEST || ray->side == EAST)
+	// not sure here what side is?, wait maybe it is side the ray is hitting
+	// 	wall_x = player->pos_y + ray->perpWallDist * ray->rayDirY;
+	// else
+	// 	wall_x = player->pos_x + ray->perpWallDist * ray->rayDirX;
+	wall_x -= floor(wall_x); // this is to get the texture x coordinate
+	line->x = current_x;     // how do l find current?
 	// paint texture if the ray hits a wall
-	line->y0 = 0;//start at top of screen
-	if (root->game->map[ray->mapX][ray->mapY] == 1) //confirm if the ray hits the wall
+	get_line_height(ray); // get the line height
+	if (root->game->map[ray->mapX][ray->mapY] == 1)
+	// confirm if the ray hits the wall
 	{
-		line->y0 = ray->draw_start; //start at top start of draw_line
-		line->y1 = ray->draw_end; //end at the end of draw_line
-		paint_texture_line(root, ray, line, wall_x); //paint the texture line using the wall_x
+		line->y0 = ray->draw_start;
+		// start at top start of draw_line
+		line->y1 = ray->draw_end;
+		// end at the end of draw_line
+		paint_texture_line(root, ray, line, wall_x);
+		// paint the texture line using the wall_x
 	}
-	get_line_height(ray); //get the line height
-	line->y1 = ray->draw_start; //end of the wall
-	paint_line(root, line, root->crgb); //paint the ceiling
-	
-	line->y0 = ray->draw_end; //we need to know the end of the wall
+	line->y0 = 0;                          // start at top of screen
+	line->y1 = ray->draw_start;            // end of the wall
+	paint_line(root, line, root->ceiling_color); // paint the ceiling
+	line->y0 = ray->draw_end;             
+		// we need to know the end of the wall
 	// start at bottom of page
 	line->y1 = WINDOW_HEIGHT;
 	// function to paint a solid color
-	paint_line(root, line, root->frgb);
+	paint_line(root, line, root->floor_color);
 }
 
-//we change the pixel of the root image data the line
-pixel_on_img(int rgb, int x, int y, t_image *img)
+// we change the pixel of the root image data the line
+// change this function to point to mlx_image, use my_mlx_put_image
+pixel_on_img(int rgb, int x, int y, t_img *mlx_img) // mlx_image
 {
-	int	r;
-	int	g;
-	int	b;
+	int r;
+	int g;
+	int b;
 
 	r = (rgb >> 16) & 0xFF;
 	g = (rgb >> 8) & 0xFF;
 	b = rgb & 0xFF;
-	img->data[y * img->line_length + x * img->bits_per_pixel / 8] = b;
-	img->data[y * img->line_length + x * img->bits_per_pixel / 8 + 1] = g;
-	img->data[y * img->line_length + x * img->bits_per_pixel / 8 + 2] = r;
+	mlx_img->data[y * mlx_img->line_length + x * mlx_img->bits_per_pixel
+		/ 8] = b;
+	mlx_img->data[y * mlx_img->line_length + x * mlx_img->bits_per_pixel / 8
+		+ 1] = g;
+	mlx_img->data[y * mlx_img->line_length + x * mlx_img->bits_per_pixel / 8
+		+ 2] = r;
 }
 
-//we change the pixel of the root image data
-void	texture_on_img(t_data *root, t_ray *ray, t_line *line, t_image *texture)
+// we change the pixel of the root image data
+void	texture_on_img(t_data *root, t_ray *ray, t_line *line, t_img *texture)
 {
 	int	scale;
-	//we dont know  the line_height
-	//we dont know texture line length
+
+	// we dont know  the line_height
+	// we dont know texture line length
 	// we dont know player cam_height
 	// we dont know
-
 	scale = line->y - (WINDOW_HEIGHT / 2) + ray->line_height / 2;
 	scale = line->y * texture->line_length - (WINDOW_HEIGHT
 			* root->game->player->cam_height) * texture->line_length / 2
@@ -139,32 +174,35 @@ void	texture_on_img(t_data *root, t_ray *ray, t_line *line, t_image *texture)
 	//              Very wack need to refer to minilibx docs
 	root->mlx_img->data[line->y + line->x * root->mlx_img->bits_per_pixel
 		/ 8] = texture->data[line->tex_y + line->tex_x
-		* (texture->bits_per_pixel / 8)];
+		* (texture->bits_per_pixel / 8)]; // we need to change the texture now
 	// and to adjust for line size
 	root->mlx_img->data[line->y * root->mlx_img->line_length + line->x
 		* root->mlx_img->bits_per_pixel / 8] = texture->data[line->tex_y
 		* texture->line_length + line->tex_x * (texture->bits_per_pixel / 8)];
+	// why? this
 }
 
-//we paint the texture line
-void	paint_texture_line(t_data *root, t_line *line, int rgb) // or paint_texture_line
+// we paint the texture line
+void	paint_texture_line(t_data *root, t_ray *ray, t_line *line, int rgb)
+// or paint_texture_line
 {
 	int y;
 	int y_max;
-	
-	y = line->y0;
+
+	line->y = line->y0;
 	y_max = line->y1;
-	if (y >= 0)
+	if (line->y >= 0)
 	{
-		while (y < y_max)
+		while (line->y < y_max)
 		{
-			texture_on_img(rgb, line->x, y, root->mlx_img);
-			y++;
+			// which image do we paint, where is the ray facing?
+			texture_on_img(root, ray, line, root->game->texture_image);
+			line->y++;
 		}
 	}
 }
 
-//we paint the texture line
+// we paint the texture line
 void	paint_line(t_data *root, t_line *line, int rgb) // or paint_texture_line
 {
 	int y;
@@ -177,6 +215,7 @@ void	paint_line(t_data *root, t_line *line, int rgb) // or paint_texture_line
 		while (y < y_max)
 		{
 			pixel_on_img(rgb, line->x, y, root->mlx_img);
+			// we may just put put pixel to image function
 			y++;
 		}
 	}
@@ -191,11 +230,17 @@ void	cast_ray(t_player *player)
 	while (++x < WINDOW_WIDTH)
 	{
 		initiate_ray(ray);
-		cameraX = 2 * x / double(WINDOW_WIDTH) - 1; //this is the x coordinate of the camera
-		ray->rayDirX = player->dirX + player->planeX * cameraX; //this is the direction of the ray
-		ray->rayDirY = player->dirY + player->planeY * cameraX; //this is the direction of the ray
-		initiate_values(ray, player); //this is the intial_values of the ray;
-		wall_position(ray); //we have the x and y of the wall;
-		draw_wall(player, ray, x)
+		cameraX = 2 * x / double(WINDOW_WIDTH) - 1;
+		// this is the x coordinate of the camera
+		ray->rayDirX = player->dirX + player->planeX * cameraX;
+		// this is the direction of the ray
+		ray->rayDirY = player->dirY + player->planeY * cameraX;
+		// this is the direction of the ray
+		initiate_values(ray, player);
+		// this is the intial_values of the ray;
+		wall_position(ray);
+		// we have the x and y of the wall;
+		get_texture(root->game); //we need to use the ray direction to find the texture to put on
+		draw_wall(player, ray, x);
 	}
 }
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tofaramususa <tofaramususa@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 19:56:47 by tmususa           #+#    #+#             */
-/*   Updated: 2024/02/13 19:13:28 by tofaramusus      ###   ########.fr       */
+/*   Updated: 2024/02/13 23:35:09 by tofaramusus      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,75 @@
 #define ROTATION_SPEED
 #define PLAYER_SPEED
 
-
+/// @brief vertical line to be drawn on the screen
 typedef struct s_line
 {
-	int x; //x coordinate of line relative to screen
-	int y; //the current pixel index of the line along y axis
-	int y0; //y start index of drawing texture
-	int y1; //y end index of drawing texture
-	int tex_x; //x coordinate of texture
-	int tex_y; //y coordinate of texture
-} t_line;
+	int x;     // x coordinate of line relative to screen
+	int y;     // the current pixel index of the line along y axis
+	int y0;    // y start index of drawing texture
+	int y1;    // y end index of drawing texture
+	int tex_x; // x coordinate of texture
+	int tex_y; // y coordinate of texture
+}				t_line;
 
-//player information
+// player information
 typedef struct s_player
 {
-	double pos_x; //initial player position
-	double pos_y; // ''
-	double dirX; //initial direction
-	double dirY; //initial direction
-	double planeX; //initial POV
-	double planeY; //initial POV
-	doubble speed; //speed of the player
-}	t_player;
+	cam_height;    // height of the camera
+	double pos_x;  // initial player position
+	double pos_y;  // ''
+	double dirX;   // initial direction
+	double dirY;   // initial direction
+	double planeX; // initial POV
+	double planeY; // initial POV
+	double speed;  // speed of the player
+}				t_player;
 
-//ray infomation
+// ray infomation
 //
 typedef struct s_ray
 {
-	rayDirX; //this the direction of the ray;
-	rayDirY; //this is the direction of the ray
-	int mapX; //position of the ray inside map
-	int mapY;  //position of the ray inside map
+	rayDirX;  // this the direction of the ray;
+	rayDirY;  // this is the direction of the ray
+	int mapX; // position of the ray inside map
+	int mapY; // position of the ray inside map
 	deltaX;
 	deltaY;
 	sideX;
 	sideY;
-	int stepX; //direction to step in X or Y
+	int stepX; // direction to step in X or Y
 	int stepY; //
-	int hit; //was a wall hit
-	int side; //was a NS or a EW wall hit
-	double perpWallDist;
-	int current_x; //current x coordinate of the ray
-	int draw_start; //start of the line to draw
-	int line_height; //height of the line to draw
-	int draw_end; //end of the line to draw
-} t_ray;
+	int hit;   // was a wall hit
+	int side;  // was a NS or a EW wall hit
+	double		perpWallDist;
+	int current_x;   // current x coordinate of the ray
+	int draw_start;  // start of the line to draw
+	int line_height; // height of the line to draw
+	int draw_end;    // end of the line to draw
+}				t_ray;
 
+typedef struct s_game
+{
+	char		**map;
+	t_player	*player;
+	// we need the image here
+	texture; // texture image
 
-//information about the whole game
-typedef s_data {
-	t_ray *ray;
-	t_player *player;
-	//we need an image too
+}			t_game;
+
+// information about the whole game
+typedef struct s_data
+{
+	t_ray		*ray;
+	// we need an image too
+	t_game		*game;
+	// t_img mlx_img; // the image we will put to window
+	ceiling_color; // ceiling color
+	floor_color; // floor color
+	// t_img north_texture; //get image
+	// t_img south_texture; //get image
+	// t_img east_texture; //get image
+	// t_img west_texture; //get image
 	
-} t_data;
 
-
-
+}				t_data;
